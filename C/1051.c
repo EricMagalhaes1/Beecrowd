68c74bba4b862c1fa5602d949466db3d2ca8105a
Eric Magalhães <kir3vv@gmail.com>
2024-10-07 17:43:56 -0300

    Problem 1051 sended

---
+++ C/1051.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main (){
+    float salario;
+    float taxa = 0;
+
+    scanf("%f", &salario);
+
+    if(salario <= 2000.00 ){
+      printf("Isento\n");
+    }
+    else if (salario > 2000.00 && salario <=3000.00){
+      taxa += ((salario - 2000)/100) * 8;
+    }
+    else if (salario > 3000.00 && salario <=4500.00){
+      taxa += ((salario - 3000)/100) * 18 + 80;
+    }
+    else if (salario > 4500.00 ){
+      taxa += ((salario - 4500)/100) * 28 + 350;
+    }
+    printf("R$ %.2f\n", taxa);
+
+    return 0;
+}
