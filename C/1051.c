eac0b72b845936fb008361cb3002167e5b701a05
Eric Magalhães <kir3vv@gmail.com>
2024-10-07 17:45:58 -0300

    Update 1051.c

--- C/1051.c
+++ C/1051.c
@@ -7,19 +7,21 @@ int main (){
 
     scanf("%f", &salario);
 
-    if(salario <= 2000.00 ){
-      printf("Isento\n");
-    }
-    else if (salario > 2000.00 && salario <=3000.00){
+
+    if (salario > 2000.00 && salario <=3000.00){
       taxa += ((salario - 2000)/100) * 8;
     }
     else if (salario > 3000.00 && salario <=4500.00){
       taxa += ((salario - 3000)/100) * 18 + 80;
     }
-    else if (salario > 4500.00 ){
+    else if (salario > 4500.00){
       taxa += ((salario - 4500)/100) * 28 + 350;
     }
+    if(taxa > 0){
     printf("R$ %.2f\n", taxa);
-
+    }
+    else{
+      printf("Isento\n");
+    }
     return 0;
 }
